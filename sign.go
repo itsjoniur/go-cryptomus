314a9a77da9130be61505ff18d0583d83e985e74
itsjoniur <ahmadamoori.dev@gmail.com>
2023-11-26 19:00:09 +0330

    add sign request method

---
+++ sign.go
@@ -0,0 +1,13 @@
+package gocryptomus
+
+import (
+	"crypto/md5"
+	"encoding/base64"
+	"encoding/hex"
+)
+
+func (c *Cryptomus) SignRequest(apiKey string, reqBody []byte) string {
+	data := base64.StdEncoding.EncodeToString(reqBody)
+	hash := md5.Sum([]byte(data + apiKey))
+	return hex.EncodeToString(hash[:])
+}
---
+++ sign_test.go
@@ -0,0 +1,26 @@
+package gocryptomus
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSignRequest(t *testing.T) {
+	httpClient := http.Client{}
+	c := New(&httpClient, "abcd", "bcda", "")
+
+	b := map[string]any{
+		"amount":   10,
+		"currency": "usd",
+		"order_id": "OrDeRiD",
+	}
+	d, err := json.Marshal(b)
+	require.NoError(t, err)
+	require.NotEmpty(t, d)
+
+	sign := c.SignRequest(c.PaymentApiKey, d)
+	require.Equal(t, "0f66e9e002a45d4285cc77a227dadfeb", sign)
+}
