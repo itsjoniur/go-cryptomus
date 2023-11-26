a0408bf1db18b61eeeee2bbd19917a68bd153d55
itsjoniur <ahmadamoori.dev@gmail.com>
2023-11-26 22:45:21 +0330

    added fetch method

--- cryptomus.go
+++ cryptomus.go
@@ -1,6 +1,10 @@
 package gocryptomus
 
-import "net/http"
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
 
 const APIURL = "https://api.cryptomus.com/v1"
 
@@ -18,4 +22,23 @@ func New(client *http.Client, merchant, paymentApiKey, payoutApiKey string) *Cry
 		paymentApiKey: paymentApiKey,
 		payoutApiKey:  payoutApiKey,
 	}
+}
+
+func (c *Cryptomus) fetch(method string, endpoint string, payload any) (*http.Response, error) {
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	sign := c.signRequest(c.paymentApiKey, body)
+	req, err := http.NewRequest("POST", APIURL+endpoint, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("merchant", c.merchant)
+	req.Header.Set("sign", sign)
+	res, err := c.client.Do(req)
+	return res, err
 }
