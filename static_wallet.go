eabfc42821b44d2d0eb92b665b6ba3d8850a10af
itsjoniur <ahmadamoori.dev@gmail.com>
2023-11-26 22:47:29 +0330

    added generate qr code and block address methods

--- static_wallet.go
+++ static_wallet.go
@@ -1,12 +1,15 @@
 package gocryptomus
 
 import (
-	"bytes"
 	"encoding/json"
-	"net/http"
+	"errors"
 )
 
-const CreateStaticWalletEndpoint = "/wallet"
+const (
+	createStaticWalletEndpoint         = "/wallet"
+	generateStaticWalletQRCodeEndpoint = "/wallet/qr"
+	blockWalletAddressEndpoint         = "/wallet/block-address"
+)
 
 type StaticWalletRequest struct {
 	Currency string `json:"currency"`
@@ -30,34 +33,80 @@ type StaticWalletResponse struct {
 	Url        string `json:"url"`
 }
 
-type StaticWalletRawResponse struct {
+type staticWalletRawResponse struct {
 	Result *StaticWalletResponse `json:"result"`
 	State  int8                  `json:"state"`
 }
 
+type staticWalletQRCodeRawResponse struct {
+	Result struct {
+		Image string `json:"image"`
+	} `json:"result"`
+	State int8 `json:"state"`
+}
+
+type BlockAddressRequest struct {
+	WalletUUID    string `json:"uuid,omitempty"`
+	OrderId       string `json:"order_id,omitempty"`
+	IsForceRefund bool   `json:"is_force_refund,omitempty"`
+}
+
+type BlockAddressResponse struct {
+	WalletUUID string `json:"uuid"`
+	Status     string `json:"status"`
+}
+
+type blockAddressRawResponse struct {
+	Result *BlockAddressResponse
+	State  int8
+}
+
 func (c *Cryptomus) CreateStaticWallet(staticWalletReq *StaticWalletRequest) (*StaticWalletResponse, error) {
-	payload, err := json.Marshal(staticWalletReq)
-	if err != nil {
-		return nil, err
-	}
-
-	sign := c.signRequest(c.paymentApiKey, payload)
-	req, err := http.NewRequest("POST", APIURL+CreateStaticWalletEndpoint, bytes.NewBuffer(payload))
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("merchant", c.merchant)
-	req.Header.Set("sign", sign)
-	res, err := c.client.Do(req)
+	res, err := c.fetch("POST", createStaticWalletEndpoint, staticWalletReq)
 	if err != nil {
 		return nil, err
 	}
 
 	defer res.Body.Close()
 
-	response := &StaticWalletRawResponse{}
+	response := &staticWalletRawResponse{}
+	if err = json.NewDecoder(res.Body).Decode(response); err != nil {
+		return nil, err
+	}
+
+	return response.Result, nil
+}
+
+func (c *Cryptomus) GenerateStaticWalletQRCode(walletUUID string) (string, error) {
+	payload := map[string]any{"wallet_address_uuid": walletUUID}
+	res, err := c.fetch("POST", generateStaticWalletQRCodeEndpoint, payload)
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+
+	response := &staticWalletQRCodeRawResponse{}
+	if err = json.NewDecoder(res.Body).Decode(response); err != nil {
+		return "", err
+	}
+
+	return response.Result.Image, nil
+}
+
+func (c *Cryptomus) BlockAddress(blockAddressReq *BlockAddressRequest) (*BlockAddressResponse, error) {
+	if blockAddressReq.WalletUUID == "" || blockAddressReq.OrderId == "" {
+		return nil, errors.New("you should pass one of required values [WalletUUID, OrderId]")
+	}
+
+	res, err := c.fetch("POST", blockWalletAddressEndpoint, blockAddressReq)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	response := &blockAddressRawResponse{}
 	if err = json.NewDecoder(res.Body).Decode(response); err != nil {
 		return nil, err
 	}
