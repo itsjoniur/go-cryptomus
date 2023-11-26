6aa0f7a6e29696d8af745f202d788e3b7ffcd26e
itsjoniur <ahmadamoori.dev@gmail.com>
2023-11-26 19:00:59 +0330

    add custom http client

--- cryptomus.go
+++ cryptomus.go
@@ -1,15 +1,19 @@
 package gocryptomus
 
-const APIURL = "https://api.cryptomus.com/v1/"
+import "net/http"
+
+const APIURL = "https://api.cryptomus.com/v1"
 
 type Cryptomus struct {
 	Merchant      string
 	PaymentApiKey string
 	PayoutApiKey  string
+	Client        *http.Client
 }
 
-func New(merchant, paymentApiKey, payoutApiKey string) *Cryptomus {
+func New(client *http.Client, merchant, paymentApiKey, payoutApiKey string) *Cryptomus {
 	return &Cryptomus{
+		Client:        client,
 		Merchant:      merchant,
 		PaymentApiKey: paymentApiKey,
 		PayoutApiKey:  payoutApiKey,
